7a8dc775313d87a26351943aac616c3ece2a843b
ulya <ulya@vsyako.ru>
2012-09-17 11:08:31 +0400

    в тейблы перефигачила

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-  windowHeight();
+  //windowHeight();
   
   $(window).resize(function(){
-    windowHeight();
+    //windowHeight();
   });
   $('.hide-block').bind('click', function(){
     hideSidebar();
