b9a151bc5cb6de25c96690ca159acf5af46db025
ulya <ulya@vsyako.ru>
2012-09-18 13:32:26 +0400

    123

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-  //windowHeight();
-  
-  $(window).resize(function(){
-    //windowHeight();
-  });
   $('.hide-block').bind('click', function(){
     hideSidebar();
   });
@@ -23,40 +18,26 @@ $(document).ready(function(){
 // пряталка сайдбара  
 function hideSidebar(){
   var $left = $('.sidebar'),
-      $right = $('.container');
+      $right = $('.container-inner'),
+      $background = $('.background');
 
-  //if($left.hasClass('hidden')){
+  if($left.hasClass('hidden')){
     $left.animate({
-        'width': 0 + 'px'
+        'margin-left': $left.css('margin-left') + 'px'
       }, 200, function(){
           $left.removeClass('hidden').addClass('visible');
         }
     );
-  /*  $right.animate({'margin-left': 220 + 'px'}, 200);
+    $right.animate({'margin-left': 220 + 'px'}, 200);
   } else{
     $left.animate({
-        'width': 0 + 'px'
+        'margin-left': (parseInt($left.css('margin-left')) - parseInt($left.width())+5) + 'px'
       }, 200, function(){
         $left.removeClass('visible').addClass('hidden');
       }
     );
-    $right.animate({'margin-left': 0 + 'px'}, 200); 
+    $right.animate({'padding-left': 5 + 'px'}, 200); 
+    $background.animate({'margin-left': -215 + 'px'}, 200);
   }
-  */
-}   
-
-// определялка высоты окна
-function windowHeight(){
-  var windowHeight = $('body').height(),
-      headerHeight = $('header').height() + 10.0 + 1.0, // 10 - paddings, 1 - border
-      leftHeight = $('.sidebar').height(),
-      rightHeight = $('.container').height(),
-      bigger;
-
-      bigger = (leftHeight > rightHeight) ? leftHeight : rightHeight;
-      bigger = (bigger > windowHeight) ? bigger : windowHeight;
-
-  $('.middle').css('height', bigger - headerHeight + 'px');
-}
-
-   
+  
+}     
