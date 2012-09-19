73e4019f32b39c96e3dacb95d58321cdf5a0a735
solodoff <solodoff@ya.ru>
2012-09-19 09:57:19 +0400

    выезжалка. убрала лишнее

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
-  $(window).resize(function(){
-    sidebarPosition();
-  });
-  
   showSidebarHintAnimation();
  
-	var sidebarWidth = $('.sidebar').width();
+  var sidebarWidth = $('.sidebar').width();
  
   $('.hide-block').on('click', function(){
     hideSidebar(sidebarWidth);
@@ -15,13 +11,13 @@ $(document).ready(function(){
     showSidebar(sidebarWidth);
   })
   
-  $('.menu-category').bind('click', function(){
+  $('.menu-category').on('click', function(){
     $(this).toggleClass('closed').toggleClass('opened');
     $(this).parent().next('ul').slideToggle(200);
     return false;
   });
   
-  $('.open-filtr').bind('click', function(){
+  $('.open-filtr').on('click', function(){
     $('.hidden-part').slideToggle(200);
   });
 });
@@ -34,37 +30,21 @@ function hideSidebar(width){
 					'margin-left': -width,
 					'width': ($(this).width() + width) + 'px'
 				}, 200, function(){
-					$hint.show();
-				}
+							$hint.show();
+						}
     );
 }     
 
 // появлялка сайдбара
 function showSidebar(width){
-	console.log(width);
 	$hint = $('.show-block-hint');
 	$('.middle').animate({
 					'margin-left': 0,
 					'width': $(this).width() + 'px'
 				}, 200, function(){
-      $hint.hide();
-    }
+						  $hint.hide();
+						}
   );
-/*
-  var $left = $('.sidebar'),
-      $right = $('.container-inner'),
-      $background = $('.background'),
-      $hint = $('.show-block-hint');
-      
-  $left.animate({
-    'margin-left': (parseInt($left.css('margin-left')) + parseInt($left.width())) + 'px'
-  }, 200, function(){
-      $hint.hide();
-    }
-  );
-  $right.animate({'padding-left': 220 + 'px'}, 200);
-  $background.animate({'margin-left': 0 + 'px'}, 200);
-  */
 }
 
 function showSidebarHintAnimation(){
@@ -75,10 +55,4 @@ function showSidebarHintAnimation(){
   $hint.mouseleave(function(){
     $(this).animate({'margin-left': -23 + 'px'}, 20);
   });
-}
-
-function sidebarPosition(){
-  var marginLeft= $(window).width();
-  
-  $('.sidebar').css('margin-left', -marginLeft + 'px');
 }
